774ab40a2fb60901eeed994a1e4de97bbe3ee12a
jd <jd@32c91515-bade-0310-a695-fcfe42eb2883>
2010-04-09 14:50:28 +0000

    added DEBIAN stuff corrected copyright mentions into srcs
    git-svn-id: svn://sub.nekodune.net/subdisj/bzoums/bozomake@584 32c91515-bade-0310-a695-fcfe42eb2883

--- src/brev.c
+++ src/brev.c
@@ -8,6 +8,31 @@
  *  jd - 27-04-2004 jdrev@disjunkt.com
  *
  */
+/* 
+ * $Id$
+ * Bozomake Copyright (C) 2002,2003 Cristelle Barillon & Jean-Daniel Pauget
+ * A whole set of building utilities
+ *
+ * bozomake@disjunkt.com  -  http://bozomake.disjunkt.com/
+ * 
+ * This file is part of Bozomake.
+ * 
+ * Loopsoids is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Loopsoids is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Loopsoids; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * you can also try the web at http://www.gnu.org/
+ */
 
 
 #include <stdio.h>
